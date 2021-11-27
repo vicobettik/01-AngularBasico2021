f92fb0e2d0b17e39507f3ec75fee66a4c3f26c80
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2021-11-27 00:23:54 -0600

    fin de la app dbz, uso de servicios

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: 'Gohan',
+    poder: 8500
+  }
+
+  constructor(private dbzService: DbzService){
+
+  }
+
+}
